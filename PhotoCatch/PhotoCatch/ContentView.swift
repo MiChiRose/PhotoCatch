ce494a194fbcbe5097cabb3fe1d74be7208816ef
Yura Menschikov <46489157+MiChiRose@users.noreply.github.com>
2020-10-20 22:39:14 +0300

    Add files via upload

---
+++ PhotoCatch/PhotoCatch/ContentView.swift
@@ -0,0 +1,161 @@
+//
+//  ContentView.swift
+//  PhotoCatch
+//
+//  Created by Yura Menschikov on 10/14/20.
+//
+
+import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
+
+class ImageSaver: NSObject {
+    
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToAlbum(image: UIImage){
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+    
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var image: Image?
+    @State private var filterIntensity = 0.5
+    
+    @State private var showingFilterSheet = false
+    @State private var showingImagePicker = false
+    @State private var showingAlert = false
+    @State private var inputImage: UIImage?
+    @State private var processedImage: UIImage?
+    
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+    
+    var body: some View {
+        
+        let intensity = Binding<Double>(
+            get: {
+                self.filterIntensity
+            },
+            set: {
+                self.filterIntensity = $0
+                self.applyProcessing()
+            }
+        )
+        
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(Color.blue)
+                    
+                    if image != nil {
+                        image?
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        Text("Tap to select a picture")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                    }
+                }
+                .onTapGesture {
+                    self.showingImagePicker = true
+                }
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: intensity)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Filter") {
+                        self.showingFilterSheet = true
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Save") {
+                        guard let processedImage = self.processedImage else { return }
+                        
+                        let imageSaver = ImageSaver()
+
+                        imageSaver.successHandler = {
+                            print("Success!")
+                        }
+
+                        imageSaver.errorHandler = {
+                            print("Oops: \($0.localizedDescription)")
+                        }
+
+                        imageSaver.writeToAlbum(image: processedImage)
+                        self.showingAlert = true
+                    }.alert(isPresented: $showingAlert, content: {
+                        Alert(title: Text("Done"), message: Text("Photo was saved"), dismissButton: .default(Text("Ok")))
+                    })
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationBarTitle("Picture Filter")
+            .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+                ImagePicker(image: self.$inputImage)
+            }
+            .actionSheet(isPresented: $showingFilterSheet) {
+                ActionSheet(title: Text("Select a filter"), buttons: [
+                    .default(Text("Crystallize")) { self.setFilter(CIFilter.crystallize()) },
+                    .default(Text("Edges")) { self.setFilter(CIFilter.edges()) },
+                    .default(Text("Gaussian Blur")) { self.setFilter(CIFilter.gaussianBlur()) },
+                    .default(Text("Pixellate")) { self.setFilter(CIFilter.pixellate()) },
+                    .default(Text("Sepia Tone")) { self.setFilter(CIFilter.sepiaTone()) },
+                    .default(Text("Unsharp Mask")) { self.setFilter(CIFilter.unsharpMask()) },
+                    .default(Text("Vignette")) { self.setFilter(CIFilter.vignette()) },
+                    .cancel()
+                ])
+            }
+        }
+    }
+        
+        func loadImage() {
+            guard let inputImage = inputImage else { return }
+            let beginImage = CIImage(image: inputImage)
+                currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+                applyProcessing()
+        }
+    
+    func applyProcessing() {
+        let inputKeys = currentFilter.inputKeys
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
+
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+            processedImage = uiImage
+        }
+    }
+    
+    func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoCatch/PhotoCatch/PhotoCatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCatchApp.swift
+//  PhotoCatch
+//
+//  Created by Yura Menschikov on 10/14/20.
+//
+
+import SwiftUI
+
+@main
+struct PhotoCatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
